ae5516f2579a6b3cfb98417a67cfa17bb56aaa11
user66 <user66@gmail.com>
2023-10-12 16:28:16 +0800

    feat: 数据库索引

--- core/models/users.go
+++ core/models/users.go
@@ -1,6 +1,9 @@
 package models
 
-import "time"
+import (
+	"gorm.io/gorm"
+	"time"
+)
 
 type Users struct {
 	Id          int
@@ -8,11 +11,11 @@ type Users struct {
 	Name        string
 	Password    string
 	Email       string
-	NowVolume   int64      `gorm:"now_volume"`
-	TotalVolume int64      `gorm:"total_volume"`
-	CreatedAt   time.Time  `gorm:"created"`
-	UpdatedAt   time.Time  `gorm:"updated"`
-	DeletedAt   *time.Time `gorm:"deleted"`
+	NowVolume   int64          `gorm:"now_volume"`
+	TotalVolume int64          `gorm:"total_volume"`
+	CreatedAt   time.Time      `gorm:"created"`
+	UpdatedAt   time.Time      `gorm:"updated"`
+	DeletedAt   gorm.DeletedAt `gorm:"deleted"`
 }
 
 func (u *Users) TableName() string {
--- database/core.sql
+++ database/core.sql
@@ -6,22 +6,21 @@ SET FOREIGN_KEY_CHECKS = 0;
 
 # 用户信息表，用于登录
 DROP TABLE IF EXISTS `users`;
-CREATE TABLE `users`
-(
-    `id`           int(11) unsigned NOT NULL AUTO_INCREMENT,
-    `identity`     varchar(36)      not null default '',
-
-    `name`         varchar(60)      not null default '',
-    `password`     varchar(128)      not null default '',
-    `email`        varchar(100)     not null default '',
-    `now_volume`   int(10)          not null default 0,
-    `total_volume` int(10)          not null default 0,
-    `created_at`   datetime                  DEFAULT NULL,
-    `updated_at`   datetime                  DEFAULT NULL,
-    `deleted_at`   datetime                  DEFAULT NULL,
-    PRIMARY KEY (`id`)
-) ENGINE = InnoDB
-  AUTO_INCREMENT = 1
-  DEFAULT CHARSET = utf8 comment '用户信息表';
+CREATE TABLE `users` (
+     `id` int unsigned NOT NULL AUTO_INCREMENT,
+     `identity` varchar(36) NOT NULL DEFAULT '',
+     `name` varchar(60) NOT NULL DEFAULT '',
+     `password` varchar(128) NOT NULL DEFAULT '',
+     `email` varchar(100) NOT NULL DEFAULT '',
+     `now_volume` int NOT NULL DEFAULT '0',
+     `total_volume` int NOT NULL DEFAULT '0',
+     `created_at` datetime DEFAULT NULL,
+     `updated_at` datetime DEFAULT NULL,
+     `deleted_at` datetime DEFAULT NULL,
+     PRIMARY KEY (`id`),
+     UNIQUE KEY `uni_name` (`name`),
+     UNIQUE KEY `uni_email` (`email`),
+     UNIQUE KEY `uni_identity` (`identity`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb3 COMMENT='用户信息表';
 
 SET FOREIGN_KEY_CHECKS = 1;
