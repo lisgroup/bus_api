4e357f9b5e8e8c4f25c053e847dc8f39236903e5
lisgroup <lisgroup@qq.com>
2023-10-12 16:37:34 +0800

    feat: 移除无用变量

--- core/define/define.go
+++ core/define/define.go
@@ -2,7 +2,6 @@ package define
 
 import (
 	"github.com/golang-jwt/jwt/v5"
-	"os"
 )
 
 const (
@@ -26,21 +25,6 @@ type UserClaim struct {
 	Name     string
 	jwt.RegisteredClaims
 }
-
-// ObjectStorageType 对象存储类型
-// 支持 minio\cos
-var ObjectStorageType = os.Getenv("ObjectStorageType")
-
-// TencentSecretKey 腾讯云对象存储
-var TencentSecretKey = os.Getenv("TencentSecretKey")
-var TencentSecretID = os.Getenv("TencentSecretID")
-var CosBucket = "https://getcharzp-1256268070.cos.ap-chengdu.myqcloud.com"
-
-// MinIOAccessKeyID MinIO 配置
-var MinIOAccessKeyID = os.Getenv("MinIOAccessKeyID")
-var MinIOAccessSecretKey = os.Getenv("MinIOAccessSecretKey")
-var MinIOEndpoint = os.Getenv("MinIOEndpoint")
-var MinIOBucket = os.Getenv("MinIOBucket")
 
 // PageSize 分页的默认参数
 var PageSize = 20
