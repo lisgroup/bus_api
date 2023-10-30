76bd36b5371ce6a1d639da53adaed2addbd494ef
lisgroup <lisgroup@qq.com>
2023-10-30 18:02:27 +0800

    feat: 极验验证码完成验证登录

--- core/internal/logic/member/user_login_logic.go
+++ core/internal/logic/member/user_login_logic.go
@@ -6,9 +6,9 @@ import (
 	"bus_api/core/internal/svc"
 	"bus_api/core/internal/types"
 	"bus_api/core/models"
+	"bus_api/core/service/gee"
 	"bus_api/core/xerror"
 	"context"
-
 	"github.com/zeromicro/go-zero/core/logx"
 )
 
@@ -27,6 +27,29 @@ func NewUserLoginLogic(ctx context.Context, svcCtx *svc.ServiceContext) *UserLog
 }
 
 func (l *UserLoginLogic) UserLogin(req *types.UserLoginRequest) (resp *types.UserResponse, err error) {
+	// 极验验证码
+	var result *gee.GeetestLibResult
+	gtLib := gee.NewGeetestLib(l.svcCtx.Config.GeeTestId, l.svcCtx.Config.GeeTestKey)
+	challenge := req.GeeTestChallenge
+	validate := req.GeeTestValidate
+	secCode := req.GeeTestSeccode
+	bypassStatus, err := l.svcCtx.Redis.Get(l.ctx, define.GeeTestBypassStatusKey).Result()
+	if err != nil {
+		// return nil, err
+		return nil, xerror.NewCodeError(xerror.RequestParamError, err.Error())
+	}
+	if bypassStatus == "success" {
+		result = gtLib.SuccessValidate(challenge, validate, secCode)
+	} else {
+		// 宕机模式，直接当做验证成功
+		result = gtLib.FailValidate(challenge, validate, secCode)
+	}
+	// 注意，不要更改返回的结构和值类型
+	if result.Status != 1 {
+		// 校验不通过
+		return nil, xerror.NewCodeError(xerror.RequestParamError, "验证码错误")
+	}
+
 	// 登录逻辑
 	// 1. 查询用户
 	user := new(models.Users)
