c28062126d3bbc3ee58e40f51ec91ff23f77f40e
lisgroup <lisgroup@qq.com>
2023-11-14 09:24:19 +0800

    feat: 全局初始化参数

--- core/internal/svc/service_context.go
+++ core/internal/svc/service_context.go
@@ -4,24 +4,35 @@ import (
 	"bus_api/core/internal/config"
 	"bus_api/core/internal/middleware"
 	"bus_api/core/service"
+	"bus_api/core/service/wechat_service"
+	"context"
 	"github.com/go-redis/redis/v8"
+	"github.com/silenceper/wechat/v2"
+	"github.com/silenceper/wechat/v2/officialaccount"
 	"github.com/zeromicro/go-zero/rest"
 	"gorm.io/gorm"
 )
 
 type ServiceContext struct {
-	Config config.Config
-	Gorm   *gorm.DB
-	Redis  *redis.Client
-	Auth   rest.Middleware
+	Config          config.Config
+	Gorm            *gorm.DB
+	Redis           *redis.Client
+	Auth            rest.Middleware
+	Wechat          *wechat.Wechat
+	OfficialAccount *officialaccount.OfficialAccount
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
+	ctx := context.Background()
+	client := service.InitRedis(c.Redis.Addr, c.Redis.Password, c.Redis.DB)
+	account := wechat_service.NewOfficialAccount(ctx, client, c)
 	service.InitConfig(c)
 	return &ServiceContext{
-		Config: c,
-		Gorm:   service.InitMysql(c.Mysql.Dsn, c.Mysql.MaxIdleConn, c.Mysql.MaxOpenConn, c.Mysql.MaxLifetime),
-		Redis:  service.InitRedis(c.Redis.Addr, c.Redis.Password, c.Redis.DB),
-		Auth:   middleware.NewAuthMiddleware().Handle,
+		Config:          c,
+		Gorm:            service.InitMysql(c.Mysql.Dsn, c.Mysql.MaxIdleConn, c.Mysql.MaxOpenConn, c.Mysql.MaxLifetime),
+		Redis:           client,
+		Auth:            middleware.NewAuthMiddleware().Handle,
+		Wechat:          account.Wechat,
+		OfficialAccount: account.OfficialAccount,
 	}
 }
