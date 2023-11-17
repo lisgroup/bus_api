5a62e4d12d228a3ff8e334e3ca4770a90c1afbd9
lisgroup <lisgroup@qq.com>
2023-11-17 09:02:12 +0800

    feat: 获取OpenPlatform实例

--- core/service/wechat_service/wechat.go
+++ core/service/wechat_service/wechat.go
@@ -7,27 +7,40 @@ import (
 	"github.com/silenceper/wechat/v2"
 	"github.com/silenceper/wechat/v2/officialaccount"
 	offConfig "github.com/silenceper/wechat/v2/officialaccount/config"
+	"github.com/silenceper/wechat/v2/openplatform"
+	openConfig "github.com/silenceper/wechat/v2/openplatform/config"
+	openOfficialAccount "github.com/silenceper/wechat/v2/openplatform/officialaccount"
 )
 
 // OfficialAccount 公众号操作样例
 type OfficialAccount struct {
-	Wechat          *wechat.Wechat
-	OfficialAccount *officialaccount.OfficialAccount
+	Wechat              *wechat.Wechat
+	OfficialAccount     *officialaccount.OfficialAccount
+	OpenPlatform        *openplatform.OpenPlatform
+	OpenOfficialAccount *openOfficialAccount.OfficialAccount
 }
+
+var wc *wechat.Wechat
 
 // InitWechat 获取wechat实例
 // 在这里已经设置了全局cache，则在具体获取公众号/小程序等操作实例之后无需再设置，设置即覆盖
 func InitWechat(ctx context.Context, conn *redis.Client) *wechat.Wechat {
-	wc := wechat.NewWechat()
+	if wc != nil {
+		return wc
+	}
+	wc = wechat.NewWechat()
 	redisCache := NewRedis(ctx, conn)
 	wc.SetCache(redisCache)
 	return wc
 }
 
 func NewOfficialAccount(ctx context.Context, conn *redis.Client, c config.Config) *OfficialAccount {
-	wc := wechat.NewWechat()
+	if wc == nil {
+		wc = wechat.NewWechat()
+		redisCache := NewRedis(ctx, conn)
+		wc.SetCache(redisCache)
+	}
 	redisCache := NewRedis(ctx, conn)
-	wc.SetCache(redisCache)
 	cfg := &offConfig.Config{
 		AppID:          c.WeChat.AppId,
 		AppSecret:      c.WeChat.AppSecret,
@@ -40,4 +53,25 @@ func NewOfficialAccount(ctx context.Context, conn *redis.Client, c config.Config
 		Wechat:          wc,
 		OfficialAccount: officialAccount,
 	}
+}
+
+func NewOpenAccount(ctx context.Context, conn *redis.Client, c config.Config) (*openplatform.OpenPlatform, *openOfficialAccount.OfficialAccount) {
+	if wc == nil {
+		wc = wechat.NewWechat()
+		redisCache := NewRedis(ctx, conn)
+		wc.SetCache(redisCache)
+	}
+	redisCache := NewRedis(ctx, conn)
+	cfg := &openConfig.Config{
+		AppID:          c.WeChat.AppId,
+		AppSecret:      c.WeChat.AppSecret,
+		Token:          c.WeChat.Token,
+		EncodingAESKey: c.WeChat.EncodingAESKey,
+		Cache:          redisCache,
+	}
+	// officialAccount := wc.GetOfficialAccount(cfg)
+	// 下面文档中提到的openPlatform都是这个变量
+	openPlatform := wc.GetOpenPlatform(cfg)
+	officialAccount := openPlatform.GetOfficialAccount(c.WeChat.AppId)
+	return openPlatform, officialAccount
 }
