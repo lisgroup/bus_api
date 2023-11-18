f692126a4969765d4b77cefa0d6bd76975986501
lisgroup <lisgroup@qq.com>
2023-11-18 13:45:39 +0800

    feat: NewServiceContext获取OpenPlatform实例

--- core/internal/svc/service_context.go
+++ core/internal/svc/service_context.go
@@ -9,30 +9,37 @@ import (
 	"github.com/go-redis/redis/v8"
 	"github.com/silenceper/wechat/v2"
 	"github.com/silenceper/wechat/v2/officialaccount"
+	"github.com/silenceper/wechat/v2/openplatform"
+	openOfficialAccount "github.com/silenceper/wechat/v2/openplatform/officialaccount"
 	"github.com/zeromicro/go-zero/rest"
 	"gorm.io/gorm"
 )
 
 type ServiceContext struct {
-	Config          config.Config
-	Gorm            *gorm.DB
-	Redis           *redis.Client
-	Auth            rest.Middleware
-	Wechat          *wechat.Wechat
-	OfficialAccount *officialaccount.OfficialAccount
+	Config              config.Config
+	Gorm                *gorm.DB
+	Redis               *redis.Client
+	Auth                rest.Middleware
+	Wechat              *wechat.Wechat
+	OfficialAccount     *officialaccount.OfficialAccount
+	OpenPlatform        *openplatform.OpenPlatform
+	OpenOfficialAccount *openOfficialAccount.OfficialAccount
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
 	ctx := context.Background()
 	client := service.InitRedis(c.Redis.Addr, c.Redis.Password, c.Redis.DB)
 	account := wechat_service.NewOfficialAccount(ctx, client, c)
+	openPlatform, openAccount := wechat_service.NewOpenAccount(ctx, client, c)
 	service.InitConfig(c)
 	return &ServiceContext{
-		Config:          c,
-		Gorm:            service.InitMysql(c.Mysql.Dsn, c.Mysql.MaxIdleConn, c.Mysql.MaxOpenConn, c.Mysql.MaxLifetime),
-		Redis:           client,
-		Auth:            middleware.NewAuthMiddleware().Handle,
-		Wechat:          account.Wechat,
-		OfficialAccount: account.OfficialAccount,
+		Config:              c,
+		Gorm:                service.InitMysql(c.Mysql.Dsn, c.Mysql.MaxIdleConn, c.Mysql.MaxOpenConn, c.Mysql.MaxLifetime),
+		Redis:               client,
+		Auth:                middleware.NewAuthMiddleware().Handle,
+		Wechat:              account.Wechat,
+		OfficialAccount:     account.OfficialAccount,
+		OpenPlatform:        openPlatform,
+		OpenOfficialAccount: openAccount,
 	}
 }
