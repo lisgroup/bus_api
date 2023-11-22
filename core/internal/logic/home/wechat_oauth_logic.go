f86ef1a97563a89a40753062431fc6e308a66634
lisgroup <lisgroup@qq.com>
2023-11-22 09:11:26 +0800

    feat: 重定向到微信oauth授权登录

--- core/internal/logic/home/wechat_oauth_logic.go
+++ core/internal/logic/home/wechat_oauth_logic.go
@@ -32,7 +32,12 @@ func (l *WechatOauthLogic) WechatOauth() (resp *types.OauthResponse, err error) 
 	// 发起网页授权
 	oauth := l.svcCtx.OpenOfficialAccount.PlatformOauth()
 	// 重定向到微信oauth授权登录
-	redirectURI := "http://www.abc.com"
-	oauth.Redirect(l.w, l.r, redirectURI, "snsapi_userinfo", "", l.svcCtx.Config.WeChat.AppId)
+	redirectURI := l.svcCtx.Config.ApiUrl + "/wechat_oauth_callback"
+	// scope 应用授权作用域，snsapi_base （不弹出授权页面直接跳转只能获取用户openid）
+	// snsapi_userinfo （弹出授权页面可通过openid拿到昵称、性别、所在地。并且， 即使在未关注的情况下，只要用户授权，也能获取其信息 ）
+	err = oauth.Redirect(l.w, l.r, redirectURI, "snsapi_userinfo", "", l.svcCtx.Config.WeChat.AppId)
+	if err != nil {
+		return nil, err
+	}
 	return
 }
