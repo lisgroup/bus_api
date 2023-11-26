cfceb64da60e1ce1f40ae81a708dde3fa3e929f9
lisgroup <lisgroup@qq.com>
2023-11-26 09:01:41 +0800

    feat: 用户表数据

--- core/models/users.go
+++ core/models/users.go
@@ -9,13 +9,13 @@ type Users struct {
 	Id          int
 	Identity    string         `gorm:"identity;size:36;unique:uni_identity"`
 	Username    string         `gorm:"username;size:60;unique:uni_username"`
-	Password    string         `gorm:"password;size:128"`
+	Password    string         `gorm:"password;size:128;default:''"`
 	Email       string         `gorm:"email;size:100;unique:uni_email"`
-	Role        string         `gorm:"role;size:100;"`
-	Openid      string         `gorm:"openid;size:100;"`  // 微信openid
-	Unionid     string         `gorm:"unionid;size:100;"` // 微信unionid
-	NowVolume   int64          `gorm:"now_volume"`
-	TotalVolume int64          `gorm:"total_volume"`
+	Role        string         `gorm:"role;size:100;default:'Customer'"`    // 角色
+	Openid      string         `gorm:"openid;size:100;default:''"`          // 微信openid
+	Unionid     string         `gorm:"unionid;size:100;default:''"`         // 微信unionid
+	NowVolume   int64          `gorm:"now_volume;default:0;type:int(11)"`   // 当前使用
+	TotalVolume int64          `gorm:"total_volume;default:0;type:int(11)"` // 总的
 	CreatedAt   time.Time      `gorm:"created_at"`
 	UpdatedAt   time.Time      `gorm:"updated_at"`
 	DeletedAt   gorm.DeletedAt `gorm:"deleted_at"`
